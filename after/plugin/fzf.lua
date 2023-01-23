c034af1cf1b8ed1bf7b2fec02de9838ee3c8f276
Atanas Ormanov <atanasormanow@gmail.com>
2023-01-23 13:14:28 +0200

    Add map for fzf builtin functions

--- after/plugin/fzf.lua
+++ after/plugin/fzf.lua
@@ -1,5 +1,6 @@
-vim.keymap.set("n", "<leader>f", "<cmd>FzfLua files<CR>")
+vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<CR>")
 vim.keymap.set("n", "<leader>gf", "<cmd>FzfLua git_files<CR>")
 vim.keymap.set("n", "<leader>/", "<cmd>FzfLua live_grep<CR>")
 vim.keymap.set("n", "<leader>*", "<cmd>FzfLua grep_cword<CR>")
 vim.keymap.set("v", "<leader>*", "<cmd>FzfLua grep_visual<CR>")
+vim.keymap.set("n", "<leader>fzf", "<cmd>FzfLua builtin<CR>")
