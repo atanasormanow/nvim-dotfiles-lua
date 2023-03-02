5339aa82cc62612326b6e086c48fd3e450c5473f
Atanas Ormanov <atanasormanow@gmail.com>
2023-03-02 22:29:53 +0200

    Configure lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -12,36 +12,40 @@ require('lualine').setup {
     always_divide_middle = true,
     globalstatus = false,
     refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
+      statusline = 600,
+      tabline = 600,
+      winbar = 600,
+    },
   },
   sections = {
     lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
-    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_b = { 'diagnostics' },
+    lualine_c = {
+      { 'filename', path = 1 },
+    },
+    lualine_x = {
+      { 'filetype', icon_only = true },
+      'encoding',
+    },
     lualine_y = { 'progress' },
-    lualine_z = { 'location' }
+    lualine_z = { 'location' },
   },
   inactive_sections = {
-    lualine_a = {},
-    lualine_b = {},
-    lualine_c = { 'filename' },
-    lualine_x = { 'location' },
-    lualine_y = {},
-    lualine_z = {}
+    lualine_a = {
+      { 'filename', path = 1 },
+    },
+    lualine_x = {
+      { 'filetype', icon_only = true },
+      'encoding',
+    },
   },
   tabline = {
     lualine_a = {
-      {
-        'tabs',
-        mode = 1
-      }
+      { 'tabs', mode = 1 },
     },
+    lualine_x = { 'diff', 'branch' },
   },
   winbar = {},
   inactive_winbar = {},
-  extensions = {}
+  extensions = {}, -- TODO: maybe use some of {fugitive, fzf, man, quickfix, toggleterm}
 }
