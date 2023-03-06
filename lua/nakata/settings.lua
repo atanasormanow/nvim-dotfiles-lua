4067b2cd49057fa8d020592736a9d6e3106e3814
Atanas Ormanov <atanasormanow@gmail.com>
2023-03-06 11:39:23 +0200

    Set makeprg for cpp to just compile with g++

--- lua/nakata/settings.lua
+++ lua/nakata/settings.lua
@@ -52,3 +52,6 @@ vim.cmd "autocmd FileType r setlocal makeprg=/usr/bin/Rscript\\ %"
 
 -- Run scheme files with racket
 vim.cmd "autocmd FileType scheme setlocal makeprg=/usr/bin/racket\\ %"
+
+-- Compile and run cpp files
+vim.cmd "autocmd FileType cpp setlocal makeprg=g++\\ %\\ -o\\ %<"
