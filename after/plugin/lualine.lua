a5ec713122b7e74ca1eaf952fd57805899469e49
Atanas Ormanov <atanasormanow@gmail.com>
2023-03-09 13:07:10 +0200

    Add more space for tabs and a flag if modified

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -34,18 +34,34 @@ require('lualine').setup {
     lualine_a = {
       { 'filename', path = 1 },
     },
-    lualine_x = {
+    lualine_z = {
       { 'filetype', icon_only = true },
       'encoding',
     },
   },
   tabline = {
     lualine_a = {
-      { 'tabs', mode = 1 },
+      {
+        'tabs',
+        mode = 1,
+        max_length = vim.o.columns * 0.8,
+        -- tabs_color = {
+        --   -- Same values as the general color option can be used here.
+        --   active = 'lualine_{section}_normal', -- Color for active tab.
+        --   inactive = 'lualine_{section}_inactive', -- Color for inactive tab.
+        -- },
+        fmt = function(name, context)
+          -- Show + if buffer is modified in tab
+          local buflist = vim.fn.tabpagebuflist(context.tabnr)
+          local winnr = vim.fn.tabpagewinnr(context.tabnr)
+          local bufnr = buflist[winnr]
+          local mod = vim.fn.getbufvar(bufnr, '&mod')
+
+          return name .. (mod == 1 and ' +' or '')
+        end
+      },
     },
-    lualine_x = { 'diff', 'branch' },
+    lualine_y = { 'diff', 'branch' },
   },
-  winbar = {},
-  inactive_winbar = {},
   extensions = {}, -- TODO: maybe use some of {fugitive, fzf, man, quickfix, toggleterm}
 }
