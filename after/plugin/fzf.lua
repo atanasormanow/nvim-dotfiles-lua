d86933135e92b73ff5657024d31023022467d217
Atanas Ormanov <atanasormanow@gmail.com>
2023-03-16 23:27:50 +0200

    Add session picker for fzf + alpha entry

--- after/plugin/fzf.lua
+++ after/plugin/fzf.lua
@@ -5,3 +5,21 @@ vim.keymap.set("n", "<leader>f/", "<cmd>FzfLua live_grep<CR>")
 vim.keymap.set("n", "<leader>f*", "<cmd>FzfLua grep_cword<CR>")
 vim.keymap.set("v", "<leader>f*", "<cmd>FzfLua grep_visual<CR>")
 vim.keymap.set("n", "<leader>fzf", "<cmd>FzfLua builtin<CR>")
+vim.keymap.set("n", "<leader>fs", "<cmd>lua _list_sessions()<CR>")
+
+function _list_sessions()
+  local sessions_dir = vim.fn.stdpath('data') .. "/sessions/"
+  require 'fzf-lua'.files({
+    prompt = "Restore Session> ",
+    cmd = "ls",
+    cwd = sessions_dir,
+    file_icons = false,
+    preview_opts = 'hidden',
+    actions = {
+      ['default'] = function(selected, opts)
+        local command = "RestoreSession " .. sessions_dir .. selected[1]
+        vim.cmd(command)
+      end
+    }
+  })
+end
