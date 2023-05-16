c1529bf99c70e8d293bb05dd19cff0598d0921e8
Atanas Ormanov <atanasormanow@gmail.com>
2023-05-16 16:30:41 +0300

    feat: add incremental selection keybinds

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,8 @@
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = {
     "help", "javascript", "typescript", "c", "cpp", "lua", "rust",
-    "haskell", "scheme", "racket"
+    "haskell", "scheme", "racket", "bash", "html", "css", "diff",
+    "markdown", "sql", "vim"
   },
 
   sync_install = false,
@@ -10,7 +11,6 @@ require 'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
     disable = { "latex" },
-
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
@@ -18,7 +18,19 @@ require 'nvim-treesitter.configs'.setup {
     additional_vim_regex_highlighting = false,
   },
 
-  -- indent = {
-  --   enable = true
-  -- },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      -- set to `false` to disable one of the mappings
+      init_selection = "gnn",
+      node_incremental = "<C-l>",
+      scope_incremental = false, -- NOTE: not sure how this works
+      node_decremental = "<C-h>",
+    },
+  },
+
+  -- NOTE: experimental, could cause a problem
+  indent = {
+    enable = true
+  },
 }
