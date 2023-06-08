22c38fc899110b1f2c21be36b916f77244e20919
Atanas Ormanov <atanasormanow@gmail.com>
2023-06-08 13:29:21 +0300

    style: mason window

---
+++ after/plugin/lsp/mason.lua
@@ -0,0 +1,12 @@
+require('mason').setup({
+  ui = {
+    icons = {
+      package_installed = "✓",
+      package_pending = "",
+      package_uninstalled = "✗"
+    },
+    border = 'double',
+    width = 0.8,
+    height = 0.8,
+  }
+})
