d8b8bdaa4b1234d2721979f8f18040065df9fb8f
Atanas Ormanov <atanasormanow@gmail.com>
2023-06-14 01:18:02 +0300

    refactor: use lua for autocmd's

--- lua/nakata/commands.lua
+++ lua/nakata/commands.lua
@@ -1,6 +1,6 @@
 vim.api.nvim_create_user_command(
   'Datetime',
-  function(_input)
+  function()
     vim.cmd "put =strftime('%a, %b %d, %X')"
   end,
   { bang = true, desc = 'Put the current timestamp under the cursor' }
@@ -8,7 +8,7 @@ vim.api.nvim_create_user_command(
 
 vim.api.nvim_create_user_command(
   'ToggleBackground',
-  function(_input)
+  function()
     local gruvbox = require("gruvbox")
     gruvbox.setup({ transparent_mode = not gruvbox.config.transparent_mode })
     vim.cmd("colorscheme gruvbox")
@@ -16,17 +16,24 @@ vim.api.nvim_create_user_command(
   { bang = true, desc = 'Toggle transparent background' }
 )
 
--- set explicit filetype for .pl
-vim.cmd "autocmd BufNewFile,BufRead *.pl setf prolog"
-
--- Run R scripts with make
-vim.cmd "autocmd FileType r setlocal makeprg=/usr/bin/Rscript\\ %"
-
 -- Run scheme files with racket
-vim.cmd "autocmd FileType scheme setlocal makeprg=/usr/bin/racket\\ %"
+vim.api.nvim_create_autocmd(
+  'FileType',
+  {
+    pattern = 'scheme',
+    command = 'setlocal makeprg=/usr/bin/racket\\ %',
+  }
+)
 
 -- Compile and run cpp files
-vim.cmd "autocmd FileType cpp setlocal makeprg=g++\\ %\\ -o\\ %<"
+vim.api.nvim_create_autocmd(
+  'FileType',
+  {
+    pattern = 'cpp',
+    command = 'setlocal makeprg=g++\\ %\\ -o\\ %<',
+  }
+)
+
 -- Enable terminal debugger for Rust
 vim.api.nvim_create_autocmd(
   'FileType',
