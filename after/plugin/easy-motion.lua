c73bd11e0b78419b470619b36f299f2d6ea7c289
Atanas Ormanov <atanasormanow@gmail.com>
2023-06-15 16:43:38 +0300

    feat: use easy-motion for search and vertical jump

--- after/plugin/easy-motion.lua
+++ after/plugin/easy-motion.lua
@@ -1,10 +1,13 @@
 -- move to {char}
 vim.keymap.set("", "<leader>F", "<Plug>(easymotion-bd-f)")
 
--- move to {char} across windows-- vim.keymap.set("", "<leader>Ff", "<Plug>(easymotion-overwin-f)")
-
 -- " move to line
 vim.keymap.set("", "<Leader>L", "<Plug>(easymotion-bd-jk)")
 
--- " move to line across windows
--- vim.keymap.set("n", "<Leader>L", "<Plug>(easymotion-overwin-line)")
+-- jump vertical lines
+vim.keymap.set("", "<Leader>j", "<Plug>(easymotion-j)")
+vim.keymap.set("", "<Leader>k", "<Plug>(easymotion-k)")
+
+-- Gif config
+vim.keymap.set("", "/", "<Plug>(easymotion-sn)")
+vim.keymap.set("o", "/", "<Plug>(easymotion-tn)")
