3c3d0531cd41abacf5921a1ba8833ab8adcd96fc
Atanas Ormanov <atanasormanow@gmail.com>
2024-06-19 13:56:56 +0300

    Add vscode maps for jumping between lint marks

--- lua/nakata/vscode.lua
+++ lua/nakata/vscode.lua
@@ -154,3 +154,15 @@ vim.keymap.set(
   function()
     vscode.call("workbench.action.toggleSidebarVisibility")
   end)
+
+-- Jump to lint marks
+vim.api.nvim_set_keymap(
+  'n', ']d',
+  '<cmd>lua vim.diagnostic.goto_next()<CR>',
+  { noremap = true, silent = true }
+)
+vim.api.nvim_set_keymap(
+  'n', '[d',
+  '<cmd>lua vim.diagnostic.goto_prev()<CR>',
+  { noremap = true, silent = true }
+)
