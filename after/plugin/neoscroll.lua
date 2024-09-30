12db988ccf7c07d202724709567f6677596182b9
Atanas Ormanov <atanasormanow@gmail.com>
2024-09-30 16:52:51 +0300

    Update neoscroll config

--- after/plugin/neoscroll.lua
+++ after/plugin/neoscroll.lua
@@ -13,27 +13,32 @@ local function setup()
     end
   end
 
-  require('neoscroll').setup({
+  Neoscroll = require('neoscroll')
+  Neoscroll.setup({
     -- All these keys will be mapped to their corresponding default scrolling animation
     mappings =
     { '<C-u>', '<C-d>', '<C-b>', '<C-f>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
-    hide_cursor = false,       -- Hide cursor while scrolling
-    stop_eof = true,           -- Stop at <EOF> when scrolling downwards
-    respect_scrolloff = false, -- Stop scrolling when the cursor reaches the scrolloff margin of the file
+    hide_cursor = false,         -- Hide cursor while scrolling
+    stop_eof = true,             -- Stop at <EOF> when scrolling downwards
+    respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
     cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-    easing_function = 'sine',  -- Default easing function
-    pre_hook = nil,            -- Function to run before the scrolling animation starts
-    post_hook = post_hook,     -- Function to run after the scrolling animation ends
-    performance_mode = false,  -- Disable "Performance Mode" on all buffers.
+    easing = 'sine',             -- Default easing function
+    pre_hook = nil,              -- Function to run before the scrolling animation starts
+    post_hook = post_hook,       -- Function to run after the scrolling animation ends
+    performance_mode = false,    -- Disable "Performance Mode" on all buffers.
   })
 
-  local t = {}
-  t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '350', 'sine', [['center-cursor']] } }
-  t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '350', 'sine', [['center-cursor']] } }
-  t['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '450', 'sine' } }
-  t['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '450', 'sine' } }
+  local keymaps = {
+    ['<C-u>'] = function() Neoscroll.ctrl_u({ duration = 350, info = 'center-cursor' }) end,
+    ['<C-d>'] = function() Neoscroll.ctrl_d({ duration = 350, info = 'center-cursor' }) end,
+    ['<C-b>'] = function() Neoscroll.ctrl_b({ duration = 450 }) end,
+    ['<C-f>'] = function() Neoscroll.ctrl_f({ duration = 450 }) end,
+  }
 
-  require('neoscroll.config').set_mappings(t)
+  local modes = { 'n', 'v', 'x' }
+  for key, func in pairs(keymaps) do
+    vim.keymap.set(modes, key, func)
+  end
 end
 
 pcall(setup)
